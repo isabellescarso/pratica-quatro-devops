3d08c561bf2cfad8c59f6618260b99ccdfee42f4
iscarso <iscarso@ford.com>
2025-09-01 20:26:41 -0300

    primeiro commit

---
+++ pratica-devops/src/main/java/com/example/pratica_devops/PraticaDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.pratica_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticaDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticaDevopsApplication.class, args);
+	}
+
+}
---
+++ pratica-devops/src/test/java/com/example/pratica_devops/PraticaDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pratica_devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticaDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
